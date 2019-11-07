9dd74b188fd9f1936a51571596bc9c0cd816964a
zyunx <zyunx@zyunx.net>
2019-11-07 23:11:02 +0800

    conf添加唯一索引, 安装脚本重构

--- application/install/controller/Index.php
+++ application/install/controller/Index.php
@@ -10,25 +10,8 @@ use think\Db;
 class Index extends Controller
 {
     public function index() {
-        Db::name('conf')->insert([
-            'key' => 'user.name',
-            'value' => 'root',
-        ]);
-
-        $passwordSalt = str_rand();
-        $password = 'root123456';
-        $password = md5($passwordSalt.$password);
-
-        Db::name('conf')->insert([
-            'key' => 'user.password',
-            'value' => $password,
-        ]);
-
-        Db::name('conf')->insert([
-            'key' => 'user.password.salt',
-            'value' => $passwordSalt,
-        ]);
-
+        $userService = model('User', 'service');
+        $userService->createUser('root', 'root');
         echo '安装成功! 务必删除此模块';
     }
 }
