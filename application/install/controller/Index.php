c4693711014c095c4ee3eb924c40d359f5c05f89
zyunx <zyunx@zyunx.net>
2019-11-07 20:29:19 +0800

    安装程序

---
+++ application/install/controller/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace app\install\controller;
+
+
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index() {
+        echo 'Install success !';
+    }
+}
