edcf4ff1b22f853f10da596f9327565d207023a1
zyunx <zyunx@zyunx.net>
2019-11-16 21:39:17 +0800

    明确服务层和逻辑层的责任

--- application/common.php
+++ application/common.php
@@ -10,6 +10,27 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+
+/**
+ * 逻辑
+ * 逻辑考虑的范围在某个子系统
+ * @param $name
+ * @return \think\Model
+ */
+function logic($name) {
+    return model($name, 'logic');
+}
+
+/**
+ * 服务
+ * 服务考虑多个系统，组合后提供给用户
+ * @param $name
+ * @return \think\Model
+ */
+function service($name) {
+    return model($name, 'service');
+}
+
 function str_rand($length = 32,
                   $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
 {
